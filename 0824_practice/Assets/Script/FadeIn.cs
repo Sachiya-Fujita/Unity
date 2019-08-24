9c5a7f9eb7e4cee54f1ad5407de4b35b9d49de20
Sachiya-Fujita <s4ch1mos@gmail.com>
2019-08-24 18:18:34 +0900

    FadeIn/Out処理

---
+++ 0824_practice/Assets/Script/FadeIn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+	float fadeSpeed = 0.02f;
+	float red, green, blue, alfa, alfa_m;
+	// Start is called before the first frame update
+	void Start()
+    {
+		red = GetComponent<Image>().color.r;
+		green = GetComponent<Image>().color.g;
+		blue = GetComponent<Image>().color.b;
+		alfa = GetComponent<Image>().color.a;
+		alfa_m = alfa;
+		alfa = 0;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(alfa <= alfa_m)
+		{
+			Fadein();
+		}
+	}
+
+	void Fadein()
+	{
+		alfa += fadeSpeed;
+		GetComponent<Image>().color = new Color(red, green, blue, alfa);
+	}
+}
---
+++ 0824_practice/Assets/Script/FadeOut.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class FadeOut : MonoBehaviour
+{
+	bool isFadeOut;
+
+	float fadeSpeed = 0.02f;
+	float red, green, blue, alfa;
+	// Start is called before the first frame update
+	void Start()
+	{
+		isFadeOut = false;
+
+		red = GetComponent<Image>().color.r;
+		green = GetComponent<Image>().color.g;
+		blue = GetComponent<Image>().color.b;
+		alfa = GetComponent<Image>().color.a;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (0 <= alfa && isFadeOut == true)
+		{
+			Fadeout();
+		}
+		else if (alfa <= 0 && isFadeOut == true)
+		{
+			SceneManager.LoadScene("MainGame");
+		}
+	}
+
+	void Fadeout()
+	{
+		alfa -= fadeSpeed;
+		GetComponent<Image>().color = new Color(red, green, blue, alfa);
+	}
+
+	public void beginFade()
+	{
+		isFadeOut = true;
+	}
+}

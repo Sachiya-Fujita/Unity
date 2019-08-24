82f9dd1e6a600332cc9c6406b922c4188e4cb3e8
Sachiya-Fujita <s4ch1mos@gmail.com>
2019-08-24 18:18:10 +0900

    Sceneの追加

---
+++ 0824_practice/Assets/Script/TitleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitleManager : MonoBehaviour
+{
+	[SerializeField]
+	private FadeOut fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Debug.Log("transition");
+			Transiton();
+		}
+	}
+
+	void Transiton()
+	{
+		fade.beginFade();
+		//SceneManager.LoadScene("MainGame");
+	}
+}
